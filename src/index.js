6b912f5acdd2941a6922795df00f0dd40254f3cf
Avinesh <176383@NTTDATA.COM>
2020-06-15 14:14:25 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom";
+import App from "./App";
+//import MyPractice from "./Practice.js"
+//import Github from "./Github.js"
+//import Appmain from "./food-app/Appmain.js"
+
+
+ReactDOM.render(<App />,document.getElementById("root"));
